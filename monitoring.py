9236835e2c44b4d93d69b64cb866d8ae8a08ae44
munris <>
2022-11-05 03:37:49 +0200

    first version: alerts for funds, pool status, slashing points. Tested on beta

---
+++ monitoring.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import os, requests
+import configparser
+import json
+
+class Monitoring():
+    def __init__(self, config_file='config.ini'):
+        self.read_config(config_file)
+        validator_data = self.get_validator_data()
+        self.check_pools(validator_data)
+
+#     def checkStats(self, validator_data):
+
+    def check_pools(self, validator_data):
+        for pool in validator_data['pools']:
+            if self.shares_to_decimal(pool['balance']) < self.funds_alert:
+                self.send_validator_alert(
+                    "FUNDS ALERT",
+                    validator_data['metadata']['moniker'],
+                    pool['pool']['name'],
+                    'Balance',
+                    str(self.shares_to_decimal(pool['balance'])) + "$KYVE"
+                )
+
+            if (pool['pool']['status'] != 'POOL_STATUS_ACTIVE'):
+                self.send_validator_alert(
+                    "STATUS ALERT",
+                    validator_data['metadata']['moniker'],
+                    pool['pool']['name'],
+                    'Status ',
+                    pool['pool']['status']
+                )
+
+            if (int(pool['points']) > 0):
+                self.send_validator_alert(
+                    "POINTS ALERT",
+                    validator_data['metadata']['moniker'],
+                    pool['pool']['name'],
+                    'Points ',
+                    pool['points']
+                )
+
+    def get_validator_data(self):
+        r = requests.get(self.api_url + '/kyve/query/v1beta1/staker/' + self.valoper)
+        data = json.loads(r.content)
+        return data['staker']
+
+    def read_config(self, config_file):
+        config = configparser.ConfigParser()
+        if os.path.exists(config_file):
+            config.read(config_file)
+        else:
+            print( f"Configuration File Does Not Exist: { config_file }")
+
+        if "API_URL" in config['VARIABLES']:
+            self.api_url = config['VARIABLES']['API_URL']
+        else:
+            self.api_url = None
+
+        if "VALOPER" in config['VARIABLES']:
+            self.valoper = config['VARIABLES']['VALOPER']
+        else:
+            self.valoper = None
+
+        if "TOKEN_FUNDS_ALERT" in config['VARIABLES']:
+            self.funds_alert = int(config['VARIABLES']['TOKEN_FUNDS_ALERT'])
+        else:
+            self.funds_alert = None
+
+        if "TELEGRAM_TOKEN" in config['VARIABLES']:
+            self.telegram_token = config['VARIABLES']['TELEGRAM_TOKEN']
+        else:
+            self.telegram_token = None
+
+        if "TELEGRAM_CHAT_ID" in config['VARIABLES']:
+            self.telegram_chat_id = config['VARIABLES']['TELEGRAM_CHAT_ID']
+        else:
+            self.telegram_chat_id = None
+
+        self.decimals = 1000000000
+        self.config = config
+
+    def send(self, msg):
+        if self.telegram_token != None and self.telegram_chat_id != None:
+            requests.post(f'https://api.telegram.org/bot{self.telegram_token}/sendMessage?chat_id={self.telegram_chat_id}&text={msg}&parse_mode=HTML')
+
+    def send_validator_alert(self, alert, moniker, pool, field, value):
+        self.send(f"<b>{alert}</b> \n<code>Validator .. {moniker} \nPool ....... {pool} \n{field} .... {value}</code>")
+
+    def shares_to_decimal(self, shares):
+        return round(float( shares ) * (1/self.decimals), 2)
+
+    def decimal_to_shares(self, amount):
+        return int( amount * self.decimals )
+
+monitoring = Monitoring()
